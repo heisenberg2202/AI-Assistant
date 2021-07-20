74c4fd1e54d3711b51c32677d6e54bced9d8f6ee
Ayush Narayan <ayushnarayan22@gmail.com>
2021-07-20 18:53:26 +0530

    Till Wikipedia

---
+++ AI_assistant.py
@@ -0,0 +1,67 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+
+
+
+
+engine = pyttsx3.init('sapi5') 
+voices = engine.getProperty('voices')
+
+# print(voices)
+
+engine.setProperty('voice',voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >=0 and hour <=12:
+        speak("Good morning!")
+
+    elif hour>=12 and hour <18:
+        speak("Good Aftenoon!")  
+
+    else:
+        speak("Good Evening")
+
+    speak("I am your AI assistant. I am here to help you")         
+
+def takeCommand():
+#It takes microphone input from user and returns string output
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+         print("Listening...")
+         r.pause_threshold = 1 
+         audio = r.listen(source)
+
+    try:
+         print('Recognising...')
+         query = r.recognize_google(audio, language='en-in')
+         print(f"You said:{query}\n")
+
+    except Exception as e:
+         print(e)   #print type as error
+         print("Say that again please....")
+         return "None"
+    return query
+
+
+if __name__== "__main__":
+    wishMe()
+    while True:       
+        query = takeCommand().lower()
+
+        #For executing tasks
+        if 'wikipedia' in query:
+            speak('Searching wikipedia....')
+            query = query.replace("wikipedia","")
+
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
